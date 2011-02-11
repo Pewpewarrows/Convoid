2c12f3e81c50c51f88cddc8726fb5f5f78aeba73
Marco Chomut <marco.chomut@gmail.com>
2011-02-10 20:09:12 -0500

    Initial commit

---
+++ src/com/pewpewarrows/convoid/Convoid.java
@@ -0,0 +1,13 @@
+package com.pewpewarrows.convoid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Convoid extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
